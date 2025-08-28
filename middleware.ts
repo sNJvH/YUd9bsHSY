42f0a32ad4c735c26a76977e81c02f9513c8eee1
sNJvH <114564644+sNJvH@users.noreply.github.com>
2025-08-28 22:04:54 +0200

    解决了 空指针异常 Bug

---
+++ middleware.ts
@@ -0,0 +1,30 @@
+import { createMiddlewareClient } from "@supabase/auth-helpers-nextjs"
+import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next()
+
+  // Create a Supabase client configured to use cookies
+  const supabase = createMiddlewareClient({ req, res })
+
+  // Refresh session if expired - required for Server Components
+  await supabase.auth.getSession()
+
+  return res
+}
+
+// Specify which routes should trigger this middleware
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - public folder
+     * - public files
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+}
